a1e284bb46a91293c80fab1ce996ad63d2889f30
Sergey Nuyanzin <snuyanzin@gmail.com>
2023-05-15 11:44:30 +0200

    Add xml format

---
+++ src/main/java/net/datafaker/datafaker_gen/formats/XmlFormat.java
@@ -0,0 +1,33 @@
+package net.datafaker.datafaker_gen.formats;
+
+import net.datafaker.transformations.XmlTransformer;
+
+import java.util.Map;
+
+public class XmlFormat implements Format<CharSequence> {
+    @Override
+    public String getName() {
+        return "xml";
+    }
+
+    @Override
+    public <IN> XmlTransformer<IN> getTransformer(Map<String, String> config) {
+        XmlTransformer.XmlTransformerBuilder<Object> builder = new XmlTransformer.XmlTransformerBuilder<>();
+        if (config == null) {
+            return (XmlTransformer<IN>) builder.build();
+        }
+        for (Map.Entry<String, String> entry : config.entrySet()) {
+            switch (entry.getKey()) {
+                case "pretty":
+                    final Object isPretty = entry.getValue();
+                    if (isPretty instanceof Boolean) {
+                        builder.pretty((Boolean) isPretty);
+                    } else {
+                        builder.pretty(Boolean.parseBoolean(entry.getValue()));
+                    }
+                    break;
+            }
+        }
+        return (XmlTransformer<IN>) builder.build();
+    }
+}
