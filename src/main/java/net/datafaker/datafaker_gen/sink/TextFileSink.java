62404c241f74750f355cbb398f6937f030e2a358
narek_karapetian <narek.karapetian93@yandex.ru>
2023-05-15 22:13:38 +1000

    Add write modes + change filename to filepath

--- src/main/java/net/datafaker/datafaker_gen/sink/TextFileSink.java
+++ src/main/java/net/datafaker/datafaker_gen/sink/TextFileSink.java
@@ -1,9 +1,12 @@
 package net.datafaker.datafaker_gen.sink;
 
+import net.datafaker.datafaker_gen.util.WriteMode;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -15,7 +18,23 @@ public class TextFileSink implements Sink {
 
     @Override
     public void run(Map<String, String> config, Function<Integer, ?> function, int numberOfLines) {
-        try (BufferedWriter bw = Files.newBufferedWriter(Path.of(config.get("filename")))) {
+        String filepath = config.get("filepath");
+        String mode = config.get("mode");
+        Path path = Paths.get(filepath);
+        switch (WriteMode.getWriteMode(mode)) {
+            case OVERWRITE: break;
+            case ERROR_IF_EXISTS:
+                if (Files.exists(path)) {
+                    throw new RuntimeException("File '" + filepath + "' already exists. " +
+                            "Current write mode is: '" + mode + "'.");
+                } else {
+                    break;
+                }
+            case UNDEFINED: throw new RuntimeException("Undefined write mode: '" + mode + "'. " +
+                    "Possible modes: " + String.join(", ", WriteMode.getLegalValues()));
+        }
+
+        try (BufferedWriter bw = Files.newBufferedWriter(path)) {
             int numberOfLinesToPrint = numberOfLines;
             int batchSize = getBatchSize(config);
             while (numberOfLinesToPrint > 0) {
