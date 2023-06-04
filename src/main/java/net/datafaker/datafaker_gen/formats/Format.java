14c272f8a57bb1570f214469d23609da1cce4f1b
Sergey Nuyanzin <sergey.nuyanzin@aiven.io>
2023-06-04 13:58:16 +0200

    Add validation phase to formats (#20)

--- src/main/java/net/datafaker/datafaker_gen/formats/Format.java
+++ src/main/java/net/datafaker/datafaker_gen/formats/Format.java
@@ -1,5 +1,6 @@
 package net.datafaker.datafaker_gen.formats;
 
+import net.datafaker.transformations.Schema;
 import net.datafaker.transformations.Transformer;
 
 import java.util.Map;
@@ -7,4 +8,6 @@ import java.util.Map;
 public interface Format<OUT> {
     String getName();
     <IN> Transformer<IN, OUT> getTransformer(Map<String, String> config);
+
+    default void validateSchema(Schema schema) {}
 }
